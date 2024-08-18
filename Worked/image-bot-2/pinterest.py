a23081647099bbef426d699995ea3f424449f042
gupta-aayushkr <gupta.aayushkr@gmail.com>
2024-08-18 23:12:24 +0530

    removed pinterest

--- Worked/image-bot-2/pinterest.py
+++ Worked/image-bot-2/pinterest.py
@@ -1,14 +1,6 @@
 import requests
 
 # Constants
-ACCESS_TOKEN = '45d824fdd808831cacc4b185a3c5832d3922179c'
-PIN_CREATE_URL = 'https://api.pinterest.com/v5/pins'
-BOARD_ID = 'PFP'
-IMAGE_URL = 'https://www.pfpgeeks.com/static/images/anime-girl-pfp/webp/anime-girl-38.webp'
-PIN_TITLE = 'Anime Girl PFP'
-PIN_DESCRIPTION = 'Anime Girl PFP'
-PIN_LINK = 'https://www.pfpgeeks.com/static/images/anime-girl-pfp/webp/anime-girl-38.webp'
-
 # Headers with Authorization token
 headers = {
     'Authorization': f'Bearer {ACCESS_TOKEN}',
